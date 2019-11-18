b6bd03589db87207cf0d9230c9e3abb304b48746
Boris Blondin <blondin.boris@gmail.com>
2019-11-18 08:59:47 +0000

    tweaks

--- Forms/HoneyPot.php
+++ Forms/HoneyPot.php
@@ -4,18 +4,11 @@ namespace Pingu\HoneyPot\Forms;
 
 use Pingu\Forms\Support\Field;
 
-class HoneyPot extends Field{
+class HoneyPot extends Field
+{
 
-	public function render(){
-		echo \FormFacade::text($this->name, null, ['id' => 'my-very-nice-email']);
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function getDefaultView()
-	{
-		return 'forms::fields.'.$this->getType();
-	}
+    public function __toString(){
+        echo \FormFacade::text($this->name, null, ['id' => 'my-very-nice-email']);
+    }
 
 }
